aa1aca6f5ada3f1b251baa54d1cac947b31edc2f
LH XoomCoder <github@xoomcoder.com>
2020-09-07 15:22:35 +0200

    add class Express

--- index.php
+++ index.php
@@ -2,3 +2,8 @@
 /**
  * Plugin Name: Xoom Express
  */
+
+if (!function_exists("add_action")) die();  // stop direct access
+
+require __DIR__. "/php/class/Express.php";
+Express::start();
---
+++ php/class/Express.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * author:  Long Hai LH
+ * date:    2020-09-07 15:18:11
+ * licence: MIT
+ */
+
+class Express
+{
+    static function start()
+    {
+        
+    }
+    //@end
+}
