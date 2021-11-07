02f0425ce0b1a1fd19640d6128df501a89ffce3e
xoomcoder <69477539+xoomcoder@users.noreply.github.com>
2021-11-07 01:59:12 +0100

    add admin page

--- php/class/Express.php
+++ php/class/Express.php
@@ -27,6 +27,65 @@ class Express
     static function install ()
     {
         add_filter("template_include", "Express::template_redirect");
+
+        if (is_admin()) {
+            Express::install_admin();
+        }
+    }
+
+    static function install_admin ()
+    {
+        add_action('admin_menu', 'Express::add_menu');
+                
+    }
+
+    static function add_menu ()
+    {
+        add_menu_page(
+            'Express',
+            'Express',
+            'manage_options',
+            'express-admin',
+            'Express::build_admin',
+            ''
+        );
+
+    }
+    
+    static function build_admin() {
+        echo 
+        <<<x
+        <style>
+        #appx textarea {
+            display:block;
+            width: calc(100% - 1rem);
+            padding: 1rem;
+            font-family:monospace;
+        }
+        </style>
+        <h1>Express</h1>
+        <div id="appx">
+            <textarea v-model="command" cols="80" rows="10"></textarea>
+            <h6>{{ command.length }}</h6>
+            <button @click="counter++">click: {{ counter }}</button>
+        </div>
+
+        <script type="module">
+        import * as Vue from 'https://cdn.jsdelivr.net/npm/vue@3.2.21/dist/vue.esm-browser.prod.js';
+        
+        const appxconfig = {
+            data() {
+              return {
+                command: '',
+                counter: 0,
+              }
+            }
+          }
+          
+        window.appx = Vue.createApp(appxconfig).mount('#appx');
+
+        </script>
+        x;
     }
 
     static function template_redirect ($template)
@@ -42,7 +101,7 @@ class Express
     {
         $now = time();
         $json = [];
-        
+
         $json["time"] = date("Y-m-d H:i:s", $now);
         $json["uri"] = $_SERVER["REQUEST_URI"];
         $json["request"] = $_REQUEST;
